c03539763f29fa9ba574c83287b61ae43f965c95
arjungautam1 <arjungautam5431@gmail.com>
2024-01-27 12:27:49 +0545

    basic frontpage setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import User from "./components /getuser/User";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />,
+    },
+    {
+      path: "/add",
+      element: "User Add page",
+    },
+    {
+      path: "/edit",
+      element: "Update User Page",
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
---
+++ client/src/components /getuser/User.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./user.css";
+import { Link } from "react-router-dom";
+
+const User = () => {
+  return (
+    <div className="userTable">
+      <Link to="/add" type="button" class="btn btn-primary">
+        Add User
+      </Link>
+      <table className="table table-bordered ">
+        <thead>
+          <tr>
+            <th scope="col">S.No.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>John Doe</td>
+            <td>john@gmail.com</td>
+            <td>Canada</td>
+            <td className="actionButtons">
+              <Link to="/add" type="button" class="btn btn-danger">
+                Delete
+              </Link>
+              <Link to={"/edit"} type="button" className="btn btn-info">
+                Update
+              </Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default User;
