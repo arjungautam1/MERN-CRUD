208a6a5385fc3b5c80b12fafdca2f8770389b0d2
arjungautam1 <arjungautam5431@gmail.com>
2024-01-29 21:36:55 +0545

    Add user Completed

--- client/src/components /getuser/User.jsx
+++ client/src/components /getuser/User.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./user.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const User = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:8000/api/users");
+      setUsers(response.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="userTable">
       <Link to="/add" type="button" className="btn btn-primary">
@@ -19,20 +30,25 @@ const User = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1.</td>
-            <td>John Doe</td>
-            <td>john@gmail.com</td>
-            <td>Canada</td>
-            <td className="actionButtons">
-              <Link to="/add" type="button" className="btn btn-danger">
-                <i className="fa-solid fa-trash"></i>
-              </Link>
-              <Link to={"/update"} type="button" className="btn btn-info">
-                <i className="fa-solid fa-pen-to-square"></i>
-              </Link>
-            </td>
-          </tr>
+          {users.map((user, index) => {
+            return (
+              <tr key={user._id}>
+                {/* To fix the error of key props {user._id} */}
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
+                <td className="actionButtons">
+                  <Link to="/add" type="button" className="btn btn-danger">
+                    <i className="fa-solid fa-trash"></i>
+                  </Link>
+                  <Link to={"/update"} type="button" className="btn btn-info">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
